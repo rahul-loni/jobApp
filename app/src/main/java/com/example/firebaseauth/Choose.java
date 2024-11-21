58624b105741893d7702eda9f38e75b423a07387
Rahul Kumar <rahul@gmail.com>
2024-11-21 08:54:05 +0545

    fiest commit

--- app/src/main/java/com/example/firebaseauth/Choose.java
+++ app/src/main/java/com/example/firebaseauth/Choose.java
@@ -1,36 +1,22 @@
 package com.example.firebaseauth;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class Choose extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_choose);
-        Button employee = findViewById(R.id.button8);
-        Button employer = findViewById(R.id.button7);
-        employee.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Choose.this,Employee.class));
-                finish();
-                Toast.makeText(Choose.this, "Successful!!", Toast.LENGTH_SHORT).show();
-            }
-        });
-        employer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Choose.this,EmployerActivity.class));
-                finish();
-                Toast.makeText(Choose.this, "Successful!!", Toast.LENGTH_SHORT).show();
-            }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
     }
 }
